bafc09e4a94fd71ce65ae0a747f42e5ec0f3c112
Aleksander Okon <aleksander.okon@medicover.pl>
2022-07-12 13:20:03 +0200

    add new project - booking app and create first variable in golang

---
+++ learning/booking_app/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print("Welcome to our conference booking application")
+	fmt.Print("Get your tickets here to attend")
+
+	var conferenceName = "Go Conference"
+	fmt.Print(conferenceName)
+}
