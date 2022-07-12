cafe8c89d866e9cd6d0ff8db8a4775634ee1d737
Aleksander Okon <aleksander.okon@medicover.pl>
2022-07-12 13:47:25 +0200

    start using println and new types of variables like const

--- learning/booking_app/main.go
+++ learning/booking_app/main.go
@@ -3,9 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("Welcome to our conference booking application")
-	fmt.Print("Get your tickets here to attend")
-
 	var conferenceName = "Go Conference"
-	fmt.Print(conferenceName)
+	const conferenceTickets = 50
+	var remainingTickets = 50
+
+	fmt.Println("Welcome to our", conferenceName, "booking application")
+	fmt.Println("We have total of", conferenceTickets, "tickets and", remainingTickets, "are still available")
+	fmt.Println("Get your tickets here to attend")
+
 }
