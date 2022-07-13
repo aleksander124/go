6b17ea3e8c2d3087ea8439b5fbe984233132f506
Aleksander Okon <aleksander.okon@medicover.pl>
2022-07-13 13:41:09 +0200

    small changes in user inmput variables

--- learning/booking_app/main.go
+++ learning/booking_app/main.go
@@ -14,12 +14,23 @@ func main() {
 	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	var userName string
+	var firstName string
+	var lastName string
+	var email string
 	var userTickets int
-	// ask user for their name
-	fmt.Scanln(&userName)
 
-	userTickets = 2
-	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
+	// ask user for their name
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName)
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
+	fmt.Println("Enter your email: ")
+	fmt.Scan(&email)
+	fmt.Println("Enter number of tikcets: ")
+	fmt.Scan(&userTickets)
+
+	// fmt.Println(&remainingTickets) use pointer to see place in memory of this var
+
+	fmt.Printf("User %v booked %v tickets.\n", firstName, userTickets)
 
 }
