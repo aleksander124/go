41cd68c3fc80d964ccafe3155a769c42d87a1d80
Aleksander Okon <aleksander.okon@medicover.pl>
2022-07-13 14:20:20 +0200

    learn about array and types

--- learning/booking_app/main.go
+++ learning/booking_app/main.go
@@ -6,7 +6,7 @@ func main() {
 	conferenceName := "Go Conference"
 	const conferenceTickets int = 50
 	var remainingTickets uint = 50
-	// uint cant be negative like '-1'
+	var bookings [50]string
 
 	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
 
@@ -30,8 +30,11 @@ func main() {
 	fmt.Scan(&userTickets)
 
 	remainingTickets = remainingTickets - userTickets
+	bookings[0] = firstName + " " + lastName
 
-	// fmt.Println(&remainingTickets) use pointer to see place in memory of this var
+	fmt.Printf("the whole array: %v\n", bookings)
+	fmt.Printf("The first value: %v\n", bookings[0])
+	fmt.Printf("Array type: %v\n", bookings)
 
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v.\n", firstName, lastName, userTickets, email)
 	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
